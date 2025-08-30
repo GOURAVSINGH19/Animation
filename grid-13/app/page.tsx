ce3037175cf0ec3f11f7f8b0422596bc1f7c620f
GOURAVSINGH19 <gouravsingh4495@gmail.com>
2025-08-30 21:29:22 +0530

    init_grid-13

---
+++ grid-13/app/page.tsx
@@ -0,0 +1,12 @@
+import { DisplayCardsDemo } from '@/Components/page'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='w-full h-screen'>
+      <DisplayCardsDemo />
+    </div>
+  )
+}
+
+export default Page
