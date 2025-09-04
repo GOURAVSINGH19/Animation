162403dcfc9aa354a949a70ef6aea6f330d765f4
GOURAVSINGH19 <gouravsingh4495@gmail.com>
2025-09-04 20:42:35 +0530

    _init_grid_16

---
+++ grid-16/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from "@/component/Card"
+
+const page = () => {
+  return (
+    <div className='w-full h-screen dark:bg-black'>
+      <Card/>
+    </div>
+  )
+}
+
+export default page
